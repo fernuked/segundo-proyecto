97cd9282da82b3a185d2d83ef29ed9ceb084d26d
fernuked <guadalupearaya50@gmail.com>
2024-09-17 22:45:41 -0300

    hola

--- src/app/modules/inicio/componentes/cards/cards.component.ts
+++ src/app/modules/inicio/componentes/cards/cards.component.ts
@@ -5,6 +5,39 @@ import { Component } from '@angular/core';
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css']
 })
-export class CardsComponent {
+export class CardComponent {
+  public info: Producto[];
+
+  constructor() {
+    this.info = [
+      {
+        id: "",
+        nombre: "Facturas X DOC",
+        precio: 6.000,
+        imagen: "https://marchelo.com.ar/wp-content/uploads/2019/04/Facturas-surtidas.jpg",
+        alt: "",
+        descripcion: "FACTURAS X DOCENA; SABORES A ELECCION."
+      },
+      {
+        id: "",
+        nombre: "Churros",
+        precio: 5.000,
+        imagen: "https://media.ambito.com/p/58e4f0b3908595d5ef951723768004b3/adjuntos/239/imagenes/040/424/0040424677/churrosjpg.jpg",
+        alt: "",
+        descripcion: "CHURROS X DOCENA O MEDIA; CON DULCE DE LECHE O SIN. <3"
+      },
+
+      {
+        id: "",
+        nombre: "Selva Negra",
+        precio: 10.000,
+        imagen: "https://content-cocina.lecturas.com/medio/2018/07/19/paso-a-paso-para-realizar-selva-negra-bizcocho-de-cacao-y-cerezas-resultado-final_d7f58c97_600x600.jpg",
+        alt: "",
+        descripcion: "SELVA NEGRA BIEN RICA."
+      }
+
+    ]
+  }
 
 }
+
--- src/app/modules/inicio/inicio.module.ts
+++ src/app/modules/inicio/inicio.module.ts
@@ -2,26 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InicioRoutingModule } from './inicio-routing.module';
-import { ComponentesComponent } from './componentes/componentes.component';
-import { CardsComponent } from './componentes/cards/cards.component';
-import { PagesComponent } from './pages/pages.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
-import { InicioprincipalComponent } from './pages/inicioprincipal/inicioprincipal.component';
-import { IniciosesionComponent } from './pages/iniciosesion/iniciosesion.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
   declarations: [
-    ComponentesComponent,
-    CardsComponent,
-    PagesComponent,
     InicioComponent,
-    InicioprincipalComponent,
-    IniciosesionComponent
   ],
   imports: [
     CommonModule,
-    InicioRoutingModule
+    InicioRoutingModule,
+    MatButtonModule,
+    MatCardModule
   ]
 })
 export class InicioModule { }
